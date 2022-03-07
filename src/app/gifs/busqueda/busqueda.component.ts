1ffe24f3bf61064123e5ba9e422e73c89b191e17
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-03-07 16:59:44 +0100

    viewChild

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar')
+  txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+    const value = this.txtBuscar.nativeElement.value;
+    console.log(value);
+    this.txtBuscar.nativeElement.value = '';
+  }
+
+}
