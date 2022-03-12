422e9a15c0271eb55f5a39e731b8b83242c3997d
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-03-12 15:30:59 +0100

    servicio gifs validaciones

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,10 +11,18 @@ export class GifsService {
   public get getHistorial() : string[] {
     return [...this._historial]; //Uso el operador Spread para romper la relación con el array _historial original para evitar problemas en posibles modificaciones 
   }
+  
+  public buscarGifs( query: string ){
 
-  public buscarGifs( query: string){
-    this._historial.unshift(query);
-    console.log(this.getHistorial);
+    query = query.trim().toLocaleLowerCase();
+    if( query.length === 0 ){ //Si viene vacío, no hago nada 
+      return;
+    }
+    if( !this._historial.includes(query) ){
+      this._historial.unshift(query); //Lo añado al principio del array
+      this._historial = this._historial.splice(0,10); //Me quedo solo con 10 elementos
+      console.log(this.getHistorial);
+    }
   }
   
 }
